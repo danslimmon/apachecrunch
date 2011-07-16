39a1b026408eb6ab2b72225b96fcbda853a04eaf
Dan Slimmon <dan@danslimmon.com>
2011-07-15 21:50:23 -0400

    Added ability to run particular tests instead of all.

--- test/runner.rb
+++ test/runner.rb
@@ -5,6 +5,14 @@ $: << ".."
 $: << "./lib"
 require 'apachecrunch'
 
+class ParticularTests
+    def self.run(tests)
+        tests.each do |test_file|
+            require test_file
+        end
+    end
+end
+
 class AllTests
     def self.run
         Dir.glob("test/test_*.rb").each do |test_file|
@@ -13,4 +21,8 @@ class AllTests
     end
 end
 
-AllTests.run
+if ARGV.length > 0
+    ParticularTests.run(ARGV)
+else
+    AllTests.run
+end
