6773bd1c20e83b013b9c75129fc5c362a1487b15
Dan Slimmon <dan@danslimmon.com>
2011-07-19 09:19:54 -0400

    Added some comments and made DerivedValueFetcher cleaner.

--- lib/element_value_fetcher.rb
+++ lib/element_value_fetcher.rb
@@ -1,6 +1,6 @@
 class ApacheCrunch
     class ElementValueFetcher
-        def initialize(raw_value_fetcher=RawValueFetcher,
+        def initialize(raw_value_fetcher_cls=RawValueFetcher,
                        derived_value_fetcher_cls=DerivedValueFetcher)
             @_RawValueFetcher = raw_value_fetcher_cls
             @_DerivedValueFetcher = derived_value_fetcher_cls
@@ -23,7 +23,13 @@ class ApacheCrunch
         end
     end
 
+
+    # Returns the value of an element that was captured straight from the log.
     class RawValueFetcher
+        # Returns the value of the Element with the given name in the given Entry.
+        #
+        # Only works for elements based on tokens that we parsed directly into the Entry.  If no
+        # matching element is found, we return nil.
         def fetch(entry, element_name)
             matching_element = entry.captured_elements.find do |element|
                 element_name == element.name
@@ -33,17 +39,20 @@ class ApacheCrunch
         end
     end
 
-    class DerivedValueFetcher
-        def fetch(entry, element_name)
-            entry.captured_elements.each do |element|
-                dr = element.derivation_rule
-                if dr.derived_elements.include?(element_name)
-                    derived_elements = dr.derive_all(element.value)
-                    return derived_elements[element_name]
-                end
-            end
 
-            nil
+    # Returns the value of an element derived from one captured directly from the log.
+    class DerivedValueFetcher
+        # Returns the value for the given name by deriving from an Element in the Entry.
+        #
+        # Returns nil if no such value can be derived.
+        def fetch(entry, element_name)
+            source_element = entry.captured_elements.find do |element|
+                element.derivation_rule.derived_elements.include?(element_name)
+            end
+            return nil if source_element.nil?
+
+            derived_elements = source_element.derivation_rule.derive_all(source_element.value)
+            derived_elements[element_name]
         end
     end
 end
