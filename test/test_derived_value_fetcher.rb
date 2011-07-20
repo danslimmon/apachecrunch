83163ddf298a11051a0f0248156413fcd8ac03bf
Dan Slimmon <dan@danslimmon.com>
2011-07-20 07:37:56 -0400

    Fixed DerivedFalueFetcher's creation of StubElements.

--- test/test_derived_value_fetcher.rb
+++ test/test_derived_value_fetcher.rb
@@ -12,16 +12,24 @@ class TestDerivedValueFetcher < Test::Unit::TestCase
     # Tests a successful fetch call
     def test_fetch
         entry = StubEntry.new
-        entry.captured_elements = [StubElement.new(StubAlphanumericFormatToken.new, "foo123"),
-                                   StubElement.new(StubDerivationSourceToken.new, "herpaderp")]
+        alnum_element = StubElement.new
+        alnum_element.populate!(StubAlphanumericFormatToken.new, "foo123")
+        ds_element = StubElement.new
+        ds_element.populate!(StubDerivationSourceToken.new, "herpaderp")
+
+        entry.captured_elements = [alnum_element, ds_element]
         assert_equal("derived from herpaderp", @inst.fetch(entry, :derived))
     end
 
     # Tests a fetch call for an element that's not derivable
     def test_fetch_missing
         entry = StubEntry.new
-        entry.captured_elements = [StubElement.new(StubAlphanumericFormatToken.new, "foo123"),
-                                   StubElement.new(StubDerivationSourceToken.new, "herpaderp")]
+        alnum_element = StubElement.new
+        alnum_element.populate!(StubAlphanumericFormatToken.new, "foo123")
+        ds_element = StubElement.new
+        ds_element.populate!(StubDerivationSourceToken.new, "herpaderp")
+
+        entry.captured_elements = [alnum_element, ds_element]
         assert_nil(@inst.fetch(entry, :missing_element))
     end
 end
