ba5933c21eae014b0a99c6cbaf8e451ea65fc6d1
Dan Slimmon <dan@danslimmon.com>
2011-07-20 08:24:47 -0400

    Fixed StubElement instantiation.

--- test/test_raw_value_fetcher.rb
+++ test/test_raw_value_fetcher.rb
@@ -12,8 +12,12 @@ class TestRawValueFetcher < Test::Unit::TestCase
     # Tests a successful fetch call
     def test_fetch
         entry = StubEntry.new
-        entry.captured_elements = [StubElement.new(StubAlphanumericFormatToken.new, "foo123"),
-                                   StubElement.new(StubNumericFormatToken.new, 54321)]
+        alnum_element = StubElement.new
+        alnum_element.populate!(StubAlphanumericFormatToken.new, "foo123")
+        num_element = StubElement.new
+        num_element.populate!(StubNumericFormatToken.new, 54321)
+
+        entry.captured_elements = [alnum_element, num_element]
         assert_equal("foo123", @inst.fetch(entry, StubAlphanumericFormatToken.new.name))
         assert_equal(54321, @inst.fetch(entry, StubNumericFormatToken.new.name))
     end
@@ -21,8 +25,12 @@ class TestRawValueFetcher < Test::Unit::TestCase
     # Tests a fetch call for an element that's not there
     def test_fetch_missing
         entry = StubEntry.new
-        entry.captured_elements = [StubElement.new(StubAlphanumericFormatToken.new, "foo123"),
-                                   StubElement.new(StubNumericFormatToken.new, 54321)]
+        alnum_element = StubElement.new
+        alnum_element.populate!(StubAlphanumericFormatToken.new, "foo123")
+        num_element = StubElement.new
+        num_element.populate!(StubNumericFormatToken.new, 54321)
+
+        entry.captured_elements = [alnum_element, num_element]
         assert_nil(@inst.fetch(entry, :missing_element))
     end
 end
